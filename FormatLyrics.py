5896f62681a5b23c2658ed119bd425308f89392e
HNT1807 <121518539+HNT1807@users.noreply.github.com>
2024-01-24 10:06:54 -0800

    FormatLyrics.py

--- FormatLyrics.py
+++ FormatLyrics.py
@@ -1,9 +1,11 @@
 import streamlit as st
+import pyperclip
 
 st.title('Lyrics Formatter')
 
 # Text area for input
 lyrics = st.text_area("Enter your lyrics here:", height=250)
+formatted_lyrics = ""
 
 # Button to format lyrics
 if st.button('Format Lyrics'):
@@ -12,3 +14,11 @@ if st.button('Format Lyrics'):
         st.text_area("Formatted Lyrics:", value=formatted_lyrics, height=250, key='output')
     else:
         st.error("Please enter some lyrics to format.")
+
+# Button to copy lyrics
+if st.button('Copy Lyrics'):
+    if formatted_lyrics:
+        pyperclip.copy(formatted_lyrics)
+        st.success("Formatted lyrics copied to clipboard!")
+    else:
+        st.error("There are no formatted lyrics to copy.")
